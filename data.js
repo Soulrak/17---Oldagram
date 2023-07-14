2d80fac590dcce9c20f2d8ca0cf522dc816ff87c
Soulrak <128169236+Soulrak@users.noreply.github.com>
2023-07-14 14:10:45 -0400

    a

---
+++ data.js
@@ -0,0 +1,38 @@
+export const posts = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "images/avatar-vangogh.jpg",
+        post: "images/post-vangogh.jpg",
+        comment: "just took a few mushrooms lol",
+        likes: 21
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "images/avatar-courbet.jpg",
+        post: "images/post-courbet.jpg",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 4
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "images/avatar-ducreux.jpg",
+        post: "images/post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152
+    },
+        {
+        name: "Carlos Olvera",
+        username: "carlos1337",
+        location: "Orlando, Florida",
+        avatar: "images/cao-avatar.jpg",
+        post: "images/cao.png",
+        comment: "@vincey1853 had me try some new food",
+        likes: 68
+    }
+]
--- index.js
+++ index.js
@@ -1,37 +1,52 @@
-const posts = [
-    {
-        name: "Vincent van Gogh",
-        username: "vincey1853",
-        location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
-        comment: "just took a few mushrooms lol",
-        likes: 21
-    },
-    {
-        name: "Gustave Courbet",
-        username: "gus1819",
-        location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
-        comment: "i'm feelin a bit stressed tbh",
-        likes: 4
-    },
-        {
-        name: "Joseph Ducreux",
-        username: "jd1735",
-        location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
-        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
-    }
-]
+import { posts } from "./data.js"
 
-// for (let i = 0; i < posts.length; i++) {
-//     let postInfo = posts[i]
-//     console.log(postInfo.name)
-// }
+const mainEl = document.querySelector(".main")
+let html = ""
+
+function renderPosts(){
+    for (let i = 0; i < posts.length; i++) {
+        let postInfo = posts[i]
+        html += `
+            <article class="post">
+                        
+                <div class="post-header">
+                    <img class="avatar" src="${postInfo.avatar}" alt="">
+                    <div class="post-info">
+                        <div>
+                            <span class="name">${postInfo.name}</span>
+                        </div>
+                        <span class="location">${postInfo.location}</span>
+                    </div>
+                </div>
+    
+                <div>
+                    <img class="post-img" src="${postInfo.post}" alt="">
+                </div>
+    
+                <div class="control-bar">
+                    <img src="images/icon-heart.png" alt="">
+                    <img src="images/icon-comment.png" alt="">
+                    <img src="images/icon-dm.png" alt="">
+                </div>
+                    
+                <div class="likes-container">
+                    <span class="likes">${postInfo.likes} likes</span>
+                </div>
+                    
+                <div class="comment-container">
+                    <p><span class="username">${postInfo.username}</span>${postInfo.comment}</p>
+                </div>
+                
+            </article>
+        `
+    }
+        mainEl.innerHTML = html
+}
+
+renderPosts()
+
+
+
 
 
 
